a32a7f71b01af047cbd91299c6060ec8a66fc85d
TheViralClovers <theviralclovers@gmail.com>
2020-11-28 11:58:50 +0530

    Folder Creation and handling of errors
    The script now makes a discord emojis folder if it wasn't already present and if a non image URL is copied, the program doesn't crash but instead instructs the user to try again

--- emojiGrabber.py
+++ emojiGrabber.py
@@ -8,6 +8,14 @@ from PIL import Image
 
 recentValue = ''
 
+#to create the directory if it doesn't already exist
+def directorycheck():
+    destination  = os.path.join(os.environ.get("USERPROFILE") , "Desktop" , "Discord emojis")
+    if not (os.path.isdir(destination)):
+        os.mkdir(destination)
+
+directorycheck()
+
 #to get the value from the clipboard
 def getClipboard():
     win32clipboard.OpenClipboard()
@@ -29,20 +37,27 @@ def getRandomFileName():
 def createResizedImage():
     fileName  = getRandomFileName()[0]
     destination = getRandomFileName()[1]
-    r = requests.get(getClipboard())
-    with open(destination, 'wb') as f:
-        f.write(r.content)    
-    image = Image.open(destination) # I have combined the function that downloads the image and resizes the image because due to getRandomFileName, it would change value as soon as its called a second time
-    image = image.resize((48, 48), Image.ANTIALIAS)
-    image.save(destination)
+    try:
+        r = requests.get(getClipboard())
+        with open(destination, 'wb') as f:
+            f.write(r.content)    
+        image = Image.open(destination) # I have combined the function that downloads the image and resizes the image because due to getRandomFileName, it would change value as soon as its called a second time
+        image = image.resize((48, 48), Image.ANTIALIAS)
+        image.save(destination)
+        status = 'Image processing done'
+    except:
+        print("lmao, try copying an image url next time")
+        status = 'Image processing Failed'
+    
+    return status
 #to resize an image
-        
+    
 clipboardData = getClipboard() 
 
 while True:
     if recentValue != getClipboard(): #this is to check if some new link has been copied
         recentValue = getClipboard()
         getRandomFileName()
-        createResizedImage()
-        print("Image processing done")
+        status = createResizedImage()
+        print(status)
     time.sleep(0.4) # if this statement wasnt included, the program would consume all of the CPUs memeory, so the while statement now happens once every 0.4s
